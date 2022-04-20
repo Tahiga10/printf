8dca7672a8333e02b7428de3845c1f94c5854a15
Tahiga10 <yoroevrard@gmail.com>
2022-04-19 17:23:41 -0700

    first test

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define FLAG_UNSIGNED  2
+#define FLAG_LOWERCASE 1
+
+
+/*function prototypes*/
+int _printf(const char *format, ...);
+
+
+#endif
